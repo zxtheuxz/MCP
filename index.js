fcd466569be92702e63452fc4deae08ee886c61a
zxtheuxz <69564011+zxtheuxz@users.noreply.github.com>
2025-04-01 00:07:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001; // Mudando de 3000 para 3001
 
 // Middleware para processar JSON
 app.use(bodyParser.json());
